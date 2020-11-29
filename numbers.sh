1b7472c8982a3d7d91631939523cef13ed7f2a89
Josh Edmondson <jedmondson@chapman.edu>
2020-11-29 10:29:47 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# numbers.sh
+# Josh Edmondson
+echo "Enter a positive integer: "
+read N
+x=1
+while [ $x -le $N ] || [ $x == $N ]
+do
+	if (( $x % 2 == 0 ))
+	then
+		echo "$x even"
+	else
+		echo "$x odd"
+
+	fi
+	x=$(( $x + 1 ))
+done
